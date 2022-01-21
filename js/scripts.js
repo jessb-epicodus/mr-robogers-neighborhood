b2dd0525c7d3e1136b996939d4b2ba9fafbf265d
jb <jessb.epicodus@gmail.com>
2022-01-21 13:39:19 -0800

    WIP: add name to result

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ function beepBoop(num) {
     for (let i=0; i <= num; i++) {
       let numString = i.toString();
         if (numString.includes("3")) {
-          numArray.push("Won't you be my neighbor?");
-        // } else if(numString.includes("2")) {
-        //   numArray.push("Boop!");
-        // } else if (numString.includes("1")) {
-        //   numArray.push("Beep!");
+          numArray.push("Won't you be my neighbor," + userName + "?");
+        } else if(numString.includes("2")) {
+          numArray.push("Boop!");
+        } else if (numString.includes("1")) {
+          numArray.push("Beep!");
         } else {
         numArray.push(i)
         }
@@ -21,6 +21,7 @@ $(document).ready(function() {
   $("#mrrob").submit(function(event) {
     event.preventDefault();
     const userNum = $("input#userNumber").val();
+    const userName = $("input#userName").val();
     $("#result").text(beepBoop(userNum));
    });
 });
