3422eecae47d9235a777cd38dcf0b467b51a5bf8
jb <jessb.epicodus@gmail.com>
2022-01-21 14:48:39 -0800

    add space between loop (just because it looks better

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ const userName = $("input#userName").val(); //UI logic in bus. logic??
     for (let i=0; i <= num; i++) {
       let numString = i.toString();
         if (numString.includes("3")) {
-          numArray.push("Won't you be my neighbor, " + userName + " ?");
+          numArray.push(" Won't you be my neighbor, " + userName + " ?");
         } else if(numString.includes("2")) {
-          numArray.push("Boop!");
+          numArray.push(" Boop!");
         } else if (numString.includes("1")) {
-          numArray.push("Beep!");
+          numArray.push(" Beep!");
         } else if ((i % 7 === 0) && (i !== 0)) {
-          numArray.push("Hello, neighbor!");
+          numArray.push(" Hello, neighbor!");
         } else {
-        numArray.push(i)
+        numArray.push(" " + i)
         }
     }
     return numArray;
