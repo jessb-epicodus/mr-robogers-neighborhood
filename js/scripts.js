c7ebe60f0a70081e4b8f1bf4e698b2993728ba59
jb <jessb.epicodus@gmail.com>
2022-01-21 13:58:33 -0800

    add image & bs jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function beepBoop(num) {
     for (let i=0; i <= num; i++) {
       let numString = i.toString();
         if (numString.includes("3")) {
-          numArray.push("Won't you be my neighbor," + userName + "?");
+          numArray.push("Won't you be my neighbor");
         } else if(numString.includes("2")) {
           numArray.push("Boop!");
         } else if (numString.includes("1")) {
@@ -21,7 +21,7 @@ $(document).ready(function() {
   $("#mrrob").submit(function(event) {
     event.preventDefault();
     const userNum = $("input#userNumber").val();
-    const userName = $("input#userName").val();
+    // const userName = $("input#userName").val();
     $("#result").text(beepBoop(userNum));
    });
 });
