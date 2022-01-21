efebf1c458fe4bd9a5d0d19053d3375851386fe0
jb <jessb.epicodus@gmail.com>
2022-01-21 14:35:24 -0800

    add conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //business logic
 function beepBoop(num) {
-const userName = $("input#userName").val();
+const userName = $("input#userName").val(); //UI logic in bus. logic??
   let numArray = [];
     for (let i=0; i <= num; i++) {
       let numString = i.toString();
@@ -10,6 +10,8 @@ const userName = $("input#userName").val();
           numArray.push("Boop!");
         } else if (numString.includes("1")) {
           numArray.push("Beep!");
+        } else if (i % 7 === 0) {
+          numArray.push("Hello, neigbhor!");
         } else {
         numArray.push(i)
         }
